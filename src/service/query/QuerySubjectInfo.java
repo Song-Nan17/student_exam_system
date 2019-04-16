20e9063527b0fea55dbac46203d7b9c70b6f4e91
Song-Nan17 <songnan17@qq.com>
2019-04-16 21:59:43 +0800

    Add QuerySubjectInfo.querySpecifiedSubject.

--- src/service/query/QuerySubjectInfo.java
+++ src/service/query/QuerySubjectInfo.java
@@ -1,6 +1,7 @@
 package service.query;
 
 import dao.Select;
+import tools.Input;
 
 import java.sql.SQLException;
 
@@ -10,12 +11,26 @@ public class QuerySubjectInfo {
             case "1.2.1":
                 queryAllSubjects();
                 break;
+            case "1.2.2":
+                querySpecifiedSubject();
+                break;
         }
 
     }
 
     public static void queryAllSubjects() throws SQLException {
-        String sql = "SELECT * FROM subject";
+        String sql = "SELECT s1.id, s1.subject, s2.name AS teacher FROM subject AS s1\n" +
+                "LEFT JOIN teacher AS s2 ON s1.teacher = s2.id;";
+        Select select = new Select();
+        select.selectSubject(sql);
+    }
+
+    public static void querySpecifiedSubject() throws SQLException {
+        System.out.println("请输入课程名：");
+        String subject = Input.getInput();
+        String sql = "SELECT s1.id, s1.subject, s2.name AS teacher FROM subject AS s1\n" +
+                "LEFT JOIN teacher AS s2 ON s1.teacher = s2.id \n" +
+                "WHERE subject = \"" + subject + "\";";
         Select select = new Select();
         select.selectSubject(sql);
     }
