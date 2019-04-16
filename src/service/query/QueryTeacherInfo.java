ad32f1a100291a1875b8204b5bfb6499e0562066
Song-Nan17 <songnan17@qq.com>
2019-04-16 22:48:24 +0800

    Add QueryTeacherInfo.querySpecifiedTeacher.

--- src/service/query/QueryTeacherInfo.java
+++ src/service/query/QueryTeacherInfo.java
@@ -1,6 +1,7 @@
 package service.query;
 
 import dao.Select;
+import tools.Input;
 
 import java.sql.SQLException;
 
@@ -10,11 +11,22 @@ public class QueryTeacherInfo {
             case "1.3.1":
                 queryAllTeachers();
                 break;
+            case "1.3.2":
+                querySpecifiedTeacher();
+                break;
         }
     }
 
     public static void queryAllTeachers() throws SQLException {
         String sql = "SELECT * FROM teacher;";
+        Select select = new Select();
+        select.selectPerson(sql);
+    }
+
+    public static void querySpecifiedTeacher() throws SQLException {
+        System.out.println("请输入要查询的教师姓名：");
+        String teacher = Input.getInput();
+        String sql = "SELECT * FROM teacher WHERE name = \"" + teacher + "\";";
         Select select = new Select();
         select.selectPerson(sql);
     }
