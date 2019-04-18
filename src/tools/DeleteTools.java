4f2b36d2b9089cf389ff7ebcaa08dda50ad2f577
Song <songnan17@qq.com>
2019-04-18 21:56:03 +0800

    DeleteTools.sureToDelete.

--- src/tools/DeleteTools.java
+++ src/tools/DeleteTools.java
@@ -9,7 +9,7 @@ public class DeleteTools {
         System.out.println("删除学生之后，该学生信息将不能恢复，是否要继续删除？\n" +
                 "1. 是\n" +
                 "2. 否");
-        if (Input.getInput().equals("1")) {
+        if (sureToDelet()) {
             System.out.println("请输入要删除的学生学号：");
             String id = Input.getInput();
             Student student = new StudentDao().selectStudentById(id);
@@ -17,7 +17,15 @@ public class DeleteTools {
                 StudentDao studentDao = new StudentDao();
                 studentDao.deleteStudentById(id);
                 System.out.println("删除学生[" + student.getName() + student.getId() + "]成功！");
+            } else {
+                System.out.println("学生[学号" + id + "]不存在！");
             }
         }
     }
+
+    public static boolean sureToDelet() {
+        return Input.getInput().equals("1");
+    }
+
+
 }
